5510f5fb9e473716b7fa5cfa167b9d68a9accbcb
Ray Chen <rayallen1996xd@gmail.com>
2021-06-29 15:07:09 +0800

    fundamental

--- read.py
+++ read.py
@@ -1,6 +1,6 @@
 data = []
 count = 0
-with open('reviews.txt' , 'r') as f:
+with open('reviews.txt' , 'r') as f:#計算資料筆數
 	for line in f:
 		data.append(line)
 		count+=1
@@ -8,8 +8,20 @@ with open('reviews.txt' , 'r') as f:
 			print(len(data))
 print('檔案讀取完畢，總共有',len(data),'筆資料')
 
-sum_len = 0
+sum_len = 0#計算資料平均長度
 for d in data:
 	sum_len+= len(d)
 
 print('留言平均長度為',(sum_len/len(data)))
+
+new = []#篩選字數小魚100字的資料
+for d in data:
+	if len(d)<100:
+		new.append(d)
+print('一共有' , len(new) , '筆資料長度小於100')
+
+good = []#篩選特定字
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有',len(good),'筆資料含good')
