3799ddd175b6826c3d15d715dd6323f558b74061
Ray Chen <rayallen1996xd@gmail.com>
2021-07-01 21:49:07 +0800

    spectrum

--- read.py
+++ read.py
@@ -8,6 +8,33 @@ with open('reviews.txt' , 'r') as f:#計算資料筆數
 			print(len(data))
 print('檔案讀取完畢，總共有',len(data),'筆資料')
 
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] +=1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] >100:
+		print(word,wc[word])
+
+while True:
+	word = input('請輸入想查找的字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word , '出現的次數為：' , wc[word])
+	else:
+		print('這個字不存在留言')
+print('感謝查詢')
+
+
+
+
+
 sum_len = 0#計算資料平均長度
 for d in data:
 	sum_len+= len(d)
@@ -25,7 +52,7 @@ for d in data:
 	if 'good' in d:
 		good.append(d)
 #也可以寫成
-#good = [d for d in data if 'good' in d]
+good = [d for d in data if 'good' in d]
 print('一共有',len(good),'筆資料含good')
 #尋找差評
 bad = ['bad' in d for d in data]
